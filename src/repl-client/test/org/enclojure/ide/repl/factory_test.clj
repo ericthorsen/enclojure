86fc9f262e1c7bdd461adfaf0cee28c91a38b689
Eric Thor <ericthor@Macintosh-3.local>
2009-10-08 12:13:45 -0400

    Cleaned up the unused code in the factory-test namespace.

--- src/repl-client/test/org/enclojure/ide/repl/factory_test.clj
+++ src/repl-client/test/org/enclojure/ide/repl/factory_test.clj
@@ -23,106 +23,9 @@
         (java.awt EventQueue)
     (org.enclojure.ide.repl DefReplWindowFactory)))
 
-(def base-lib-path "/Users/ericthor/Dev/enclojure/src/repl-client/lib-artifacts/")
-
-(def libs ["clojure-contrib.jar"
-            "clojure.jar"
-            "commons-exec.jar"
-            "commons-io.jar"
-            "commons-logging.jar"
-            "repl-server.jar"
-            "slf4j-api.jar"
-            "slf4j-jdk14.jar"
-            "swing-layout.jar"])
-
-(def -history-viewers-
-  (ref {}))
-
-(defn- get-pref-file-base
-  "Given a config category, returns a path for storing/retrieving config data for the given category"
-  []
-  (let [env (into {} (System/getenv))
-        home (or (env "USERPROFILE") (env "user.home") (env "netbeans.user")
-               (env "HOME") (env "HOMEPATH"))]
-        (str home (File/separator) ".netbeans" (File/separator) "enclojure-prefs")))
-
-(defn- get-pref-file-path
-  "Given a config category, returns a path for storing/retrieving config data for the given category"
-  [config-category]
-  (let [base-path (File. (get-pref-file-base))
-        pfile (File. base-path config-category)]
-    (when-not (.exists pfile)
-      (.mkdirs base-path)
-      (.createNewFile pfile)
-      (with-open [out (OutputStreamWriter. (FileOutputStream. pfile))]
-        (binding [*out* out]
-          (prn {}))))
-    (.getCanonicalPath pfile)))
-
-(defn test-cp
-  []
-  (apply str
-    (interpose java.io.File/pathSeparator
-      (map #(format "%s%s" base-lib-path %1) libs))))
-
-(def -repl-window-factory-
-  (proxy [IReplWindowFactory][]
-    (makeReplWindow [repl-panel repl-context]
-      (let [repl-id (:repl-id repl-context)
-            repl-tc (top-window repl-id repl-panel)]
-        (proxy [IReplWindow][]
-            (getComponent [] repl-tc)
-            (open [] (.setVisible repl-tc true))
-            (makeActive []  (.setSize repl-tc 1000 1000)
-                            (.setVisible repl-tc true)
-                repl-tc)
-            (showHistory []
-                    (let [log-file (.getHistoryLogFile this)]
-                        (when (.exists (File. log-file))
-                          (let [edit-win (javax.swing.JEditorPane.)
-                                file-data (slurp log-file)
-                                document (.getDocument edit-win)]
-                            (.insertString document 0 file-data nil)
-                            (let [w (top-window (str "History for " repl-id)
-                                      edit-win)]
-                                (.setSize w 1000 1000)
-                                (.setVisible w true) w)))))
-            (getHistoryLogFile []
-                (get-pref-file-path
-                  (str repl-id "-command-history.clj")))
-          )))))
-
-(def *test-context* (assoc (merge repl-data/-repl-context-external-managed-validation-
-                      repl-data/-default-repl-data-)
-                     :classpath (System/getProperty "java.class.path")
-                     :startup-expr ""))
-
-(defn test-repl
-  []
-  (let [{repl-id :repl-id :as repl-context}
-        (assoc (merge repl-data/-repl-context-external-managed-validation-
-                      repl-data/-default-repl-data-)
-                     :classpath (test-cp)
-                     :startup-expr "")
-        irepl
-            (create-managed-external-repl
-                   (assoc
-                    (merge
-                        repl-data/-repl-context-external-managed-validation-
-                        repl-data/-default-repl-data-)
-                     :classpath (test-cp)
-                     :startup-expr "")
-              (DefReplWindowFactory.))]
-    (.setResetReplFn
-            (.getReplPanel irepl)
-            #(do (repl-manager/stop-internal-repl repl-id) (test-repl)))
-      (repl-panel/evaluate-in-repl repl-id
-        (str (repl-manager/get-settings-set-expression repl-id)))
-        (-> irepl .getReplWindow .open)
-        (-> irepl .getReplWindow .makeActive)))
-
 
 (defn test-create-in-proc-repl
+  "Sample function to create an in-proc REPL"
   []
   (let [latch (java.util.concurrent.CountDownLatch. 1)
         irepl (atom nil)
@@ -139,6 +42,8 @@
     (.await latch)))
 
 (defn test-connect-external-repl
+  "This funtion assumes there is already a process running and will connect
+using the create-unmanaged-external-repl function"
   [server-port]
   (let [latch (java.util.concurrent.CountDownLatch. 1)
         irepl (atom nil)
@@ -158,8 +63,9 @@
          (.setVisible frame true)))
         (.await latch)))
   
-
 (defn test-create-managed-repls
+  "Sample function that creates 3 managed JVM processes and connects a
+ReplPanel to each one"
   []
     (let [latch (java.util.concurrent.CountDownLatch. 1)
         irepl (atom nil)
