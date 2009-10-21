f7751824a5012d80a16c290ed383e1ec9606bb0e
Eric Thor <ericthor@Macintosh-2.local>
2009-10-21 17:36:24 -0400

    Building version of the WORK-IN-PROGRESS...

--- src/ide/src/org/enclojure/ide/analyze/parser_comp.clj
+++ src/ide/src/org/enclojure/ide/analyze/parser_comp.clj
@@ -13,44 +13,45 @@
 
 (ns #^{ :author "Eric Thorsen",
         :doc "org.enclojure.ide.analyze.parser-comp"}
-    org.enclojure.ide.analyze.parser-comp
-  (:import (org.codehaus.jparsec Indentation OperatorTable Parser Parsers
-             Scanners Terminals Token Tokens)
-    (org.codehaus.jparsec.functors Binary Map Map2 Map3 Map4 Map5 Unary Maps
-      Pair Tuples Tuple3 Tuple4 Tuple5)
-    (org.codehaus.jparsec.pattern  CharPredicate CharPredicates Pattern Patterns)
-    (org.codehaus.jparsec.misc Mapper)
-    (org.codehaus.jparsec.error ParseErrorDetails Location ParserException)
-    (java.util List ArrayList Collection)
-  ))
-
-(def  OPERATORS (into-array ["+", "-", "*", "/", ">", "<", ">=", "<="]))
-(def KEYWORDS (into-array ["interface", "class", "enum",
-                           "private", "public", "protected"]))
-(def TERMINALS (Terminals/caseSensitive OPERATORS KEYWORDS))
-
-(defn adjacent
-  ([#^Parser parser #^String name]
-    (let [find-match (fn [#^java.util.List tokens]
-                    (let [p (loop [offset (.index (.get tokens 0))
-                                  tokens tokens]
-                              (when-let [token (first tokens)]
-                                (if (not= offset (.index token))
-                                    (Parsers/expect name)
-                                (recur (+ offset (.length token))
-                                    (rest tokens)))))]
-                            (or p (Parsers/always))))
-        parse-map (proxy [Parser][]
-                    (map [#^java.util.List tokens]
-                      (if (pos? (count tokens))
-                        (find-match tokens)
-                      (Parsers/always))))]
-        (-> (.next parser parse-map)
-            .atomic .source .token)))
-  ([#^String operator]
-    (adjacent 
-      (reduce #(conj %1 (.token TERMINALS #^String %2)) [] operator) operator)))
-
-(def UNSIGNED_RIGHT_SHIFT (adjacent ">>>"))
-(def RIGHT_SHIFT (.next (-> UNSIGNED_RIGHT_SHIFT .not) (adjacent(">>"))))
+    org.enclojure.ide.analyze.parser-comp)
+;
+;  (:import (org.codehaus.jparsec Indentation OperatorTable Parser Parsers
+;             Scanners Terminals Token Tokens)
+;    (org.codehaus.jparsec.functors Binary Map Map2 Map3 Map4 Map5 Unary Maps
+;      Pair Tuples Tuple3 Tuple4 Tuple5)
+;    (org.codehaus.jparsec.pattern  CharPredicate CharPredicates Pattern Patterns)
+;    (org.codehaus.jparsec.misc Mapper)
+;    (org.codehaus.jparsec.error ParseErrorDetails Location ParserException)
+;    (java.util List ArrayList Collection)
+;  ))
+;
+;(def  OPERATORS (into-array ["+", "-", "*", "/", ">", "<", ">=", "<="]))
+;(def KEYWORDS (into-array ["interface", "class", "enum",
+;                           "private", "public", "protected"]))
+;(def TERMINALS (Terminals/caseSensitive OPERATORS KEYWORDS))
+;
+;(defn adjacent
+;  ([#^Parser parser #^String name]
+;    (let [find-match (fn [#^java.util.List tokens]
+;                    (let [p (loop [offset (.index (.get tokens 0))
+;                                  tokens tokens]
+;                              (when-let [token (first tokens)]
+;                                (if (not= offset (.index token))
+;                                    (Parsers/expect name)
+;                                (recur (+ offset (.length token))
+;                                    (rest tokens)))))]
+;                            (or p (Parsers/always))))
+;        parse-map (proxy [Parser][]
+;                    (map [#^java.util.List tokens]
+;                      (if (pos? (count tokens))
+;                        (find-match tokens)
+;                      (Parsers/always))))]
+;        (-> (.next parser parse-map)
+;            .atomic .source .token)))
+;  ([#^String operator]
+;    (adjacent
+;      (reduce #(conj %1 (.token TERMINALS #^String %2)) [] operator) operator)))
+;
+;(def UNSIGNED_RIGHT_SHIFT (adjacent ">>>"))
+;(def RIGHT_SHIFT (.next (-> UNSIGNED_RIGHT_SHIFT .not) (adjacent(">>"))))
                       
